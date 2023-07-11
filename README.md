270e1405f5c10dcb8d2cdff40c2cc80aa53ed66c
Joperezc <EFEINJPerez@outlook.com>
2023-07-11 17:15:18 -0400

    First gitHub upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# BlockCreator
+Comment block automation for functions and classes in python
+
+# Python Versions
+* Tested on Python 3.10.6
+
+# OS
+* Tested on Linux/Ubuntu
+* Mac/Windows not tested
+
+# Goals
+* Add comment blocks to functions (DONE)
+* Add comment blocks to Classes (DONE)
+* Add comment blocks to if __name__ = __main__ (DONE)
+* Add header comment block
+* Add terminal interface for entering comments without having
+  to go into IDE.
+* Correct indentation for comment blocks (DONE)
+
+
+# Instructions for Use
+1. Run the script through the linux cmd
+    * ```python3 commentBlockCreator.py (linux)```
+3. type in the file path for the python file
+   you wish to add comments to or use tab completion
---
+++ src/commentBlockCreator.py
@@ -0,0 +1,123 @@
+
+
+
+
+import sys, os
+import re
+from prompt_toolkit import completion, prompt
+
+
+
+#################
+#Comment Block
+#Function: grabFile
+#Description: Open file and call LookForFunctions
+#replace the original file with the new comment block full
+#file.
+#################
+
+def grabFile(file):
+
+    with open(file, "r") as f:
+        with open("outfile.py", "w+") as o:
+            lineNumLis = lookForFunctions(f, o)
+            os.replace(o.name, file)
+    return
+
+
+
+#################
+#Comment Block
+#Function: lookForFunctions
+#Description: Look for functions and classes,
+#and copy/write lines and comment blocks to temp
+#outfile
+#################
+
+def lookForFunctions(fileDesc, outFileDesc):
+    content = fileDesc.readlines()
+    params = ""
+    
+    for line in content:
+        if "def " in line or "class " in line or "if __name" in line:
+            splitLine = re.split("d", line)
+            try:
+                params = re.search('\(([^)]+)', line).group(1)
+            except:
+                pass
+            
+            indent = indentCalc(splitLine)
+                
+            outFileDesc.write(f"\n{indent}#################\n")
+            if len(params.split(", ")) > 2 and params != None:
+                outFileDesc.write(f"{indent}#Comment Block\n")
+                if("def" in line):
+                    lineSplit = line.split("(")[0].split(" ")
+                    outFileDesc.write(indent + f"#Function: {lineSplit[len(lineSplit) - 1]}\n")
+                elif("class" in line):
+                    lineSplit = line.split("(")[0].split(" ")[1].split(":")[0]
+                    outFileDesc.write(f"{indent}#Class: {lineSplit}\n")
+                outFileDesc.write(f"{indent}#Description: \n")
+                for i in params.split(", "):
+                    if i != "self":
+                        outFileDesc.write(f"{indent}#{i} Param: \n")
+
+            else:
+                outFileDesc.write(f"{indent}#Comment Block\n")
+                if("def" in line):
+                    lineSplit = line.split("(")[0].split(" ")
+                    outFileDesc.write(f"{indent}#Function: {lineSplit[len(lineSplit) - 1]}\n")
+                elif("class" in line):
+                    lineSplit = line.split("(")[0].split(" ")[1].split(":")[0]
+                    outFileDesc.write(f"{indent}#Class: {lineSplit}\n")
+                outFileDesc.write(f"{indent}#Description: \n")
+            
+            outFileDesc.write(indent + "#################\n\n")
+            
+        outFileDesc.write(line)
+
+
+
+#################
+#Comment Block
+#Function: indentCalc
+#Description: Calculate the indentation of the 
+#comment block based on the amount of \t or spaces
+#in the split lines
+#################
+def indentCalc(splitLine):
+    defIndent = 0
+    print(splitLine)
+    if(len(splitLine) > 1 or "\t" in splitLine[0]):
+        print(splitLine)
+        if(len(splitLine) == 1):
+            for i in splitLine[0]:
+                if i == "\t":
+                    defIndent = defIndent + 1
+        else:
+            if("\t" in splitLine[0]):
+                for i in splitLine[0]:
+                    defIndent = defIndent + 1
+            else:
+                defIndent = int(len(splitLine[0]) / 4)
+            
+        indent = ""
+        print(defIndent)
+        for i in range(defIndent):
+            indent = indent + "\t"
+        return indent
+    else:
+        return ""
+
+
+
+
+#################
+#Comment Block
+#Description: Runs main when program called
+#with prompt_toolkit allowing for tab completion
+#################
+
+if __name__ == "__main__":
+    ret = prompt("File Path: ", completer = completion.PathCompleter())
+    grabFile(ret)
